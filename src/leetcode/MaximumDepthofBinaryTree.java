55b30f724ed529d66bf5bbd6cb483e594e263b08
sherxon <sheraliobidov1199@gmail.com>
2016-12-24 15:42:08 -0600

    minmaxBinaryTree

---
+++ src/leetcode/MaximumDepthofBinaryTree.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * Created by sherxon on 12/24/16.
+ */
+public class MaximumDepthofBinaryTree {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int maxDepth(TreeNode root) {
+        if(root==null)return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+    }
+
+     // Definition for a binary tree node.
+
+      public static class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+      }
+}
