2fe2fe3051c815f190063a327c0815054427ef23
sherxon <sheraliobidov1199@gmail.com>
2016-12-29 19:03:00 -0600

    dp, range binary search, tree count

---
+++ src/InterviewQuestions/HouseRobber.java
@@ -0,0 +1,17 @@
+package InterviewQuestions;
+
+/**
+ * Created by sherxon on 12/29/16.
+ */
+public class HouseRobber {
+    public int rob(int[] a) {
+        if(a==null || a.length==0)return 0;
+        if(a.length==1) return a[0];
+        a[1]=Math.max(a[0], a[1]);
+        for(int i=2; i<a.length; i++){
+            a[i]=Math.max(a[i]+a[i-2], a[i-1]);
+        }
+
+        return a[a.length-1];
+    }
+}
