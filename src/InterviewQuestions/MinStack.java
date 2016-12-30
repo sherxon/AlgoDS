a3135f152c84a2fa7311434399eb387c975f922e
sherxon <sheraliobidov1199@gmail.com>
2016-12-30 00:44:37 -0600

    stack, dp, binarytree

---
+++ src/InterviewQuestions/MinStack.java
@@ -0,0 +1,41 @@
+package InterviewQuestions;
+
+import java.util.Stack;
+
+/**
+ * Created by sherxon on 12/29/16.
+ */
+public class MinStack {
+    Stack<Integer> stack= new Stack<>();
+    int min=Integer.MAX_VALUE;
+    /** initialize your data structure here. */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        if(x<min)min=x;
+        stack.push(x);
+
+    }
+
+    public void pop() {
+        int p=stack.peek();
+        stack.pop();
+        if(p==min){
+            min=Integer.MAX_VALUE;
+            for(Integer i:stack){
+                min=Math.min(i, min);
+            }
+        }
+
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
---
+++ src/InterviewQuestions/SumofLeftLeaves.java
@@ -0,0 +1,15 @@
+package InterviewQuestions;
+
+/**
+ * Created by sherxon on 12/29/16.
+ */
+public class SumofLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null)return 0;
+        int ls=sumOfLeftLeaves(root.left);
+        int rs=sumOfLeftLeaves(root.right);
+        if(root.left!=null && root.left.left==null && root.left.right==null)
+            ls+=root.left.val;
+        return ls+rs;
+    }
+}
