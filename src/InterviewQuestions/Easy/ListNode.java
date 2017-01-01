66ec04969474ee96962f936619d7102afa5c6fa2
sherxon <sheraliobidov1199@gmail.com>
2016-12-31 22:50:43 -0600

    odd even reorder, inserion sort in lists medium problems

--- src/InterviewQuestions/Easy/ListNode.java
+++ src/InterviewQuestions/Easy/ListNode.java
@@ -4,9 +4,9 @@ package InterviewQuestions.Easy;
  * Created by sherxon on 12/28/16.
  */
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
+   public int val;
+  public   ListNode next;
+   public ListNode(int x) {
         val = x;
         next = null;
     }
---
+++ src/InterviewQuestions/Medium/RemoveDuplicatesFromList2.java
@@ -0,0 +1,27 @@
+package InterviewQuestions.Medium;
+
+import InterviewQuestions.Easy.ListNode;
+
+/**
+ * Created by sherxon on 12/31/16.
+ */
+public class RemoveDuplicatesFromList2 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null)return null;
+        if(head.next==null)return head;
+        ListNode a=new ListNode(0);
+        ListNode root=a;
+        ListNode x=head;
+        while(x!=null){
+            ListNode y=x;
+            while(y!=null &&  y.val==x.val)y=y.next;
+            if(x.next==y){
+                ListNode temp=new ListNode(x.val);
+                a.next=temp;
+                a=temp;
+            }
+            x=y;
+        }
+        return root.next;
+    }
+}
