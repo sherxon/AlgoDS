2ac7aae7349ba3c51c6d2e5beb192ba93e294087
sherxon <sheraliobidov1199@gmail.com>
2017-01-08 17:36:26 -0600

    bellman ford, contest 14,

--- src/algo/graph/BellmanFord.java
+++ src/algo/graph/BellmanFord.java
@@ -10,6 +10,12 @@ import java.util.List;
 /**
  * Created by sherxon on 1/7/17.
  */
+/**
+* This Bellman Ford shortest path algorithm. It works with negative edges and and if there negative cycle
+ * the algorithm reports. Time complexity is O(V*E) if E is V^2 , we can say that O(V^3).
+ * This is slower than Dijkstra shortest path algorithm which works for only non-negative edges in O(VLogV)
+ * with fibonacci heap.
+* */
 public class BellmanFord<V, E extends Number>  {
     WeightedGraph<V, E> graph;
 
@@ -18,13 +24,20 @@ public class BellmanFord<V, E extends Number>  {
     }
 
    public void shortestPath(V v){
-       // initialize
+       /**
+       * Step 1:
+       * Initialization  step
+       * */
        Vertex<V> source=graph.getVertex(v);
        for (Vertex<V> vertex : graph.getVertices()) {
            if(source==vertex)vertex.setWeight(0);
            else vertex.setWeight(Integer.MAX_VALUE);
        }
-       // shortest path
+       /**
+       * Step 2:
+       *Relax all edges |V|-1 times. shortest path from source to any vertex can be found in |V|-1 iteration.
+       *
+       * */
        List<Edge> edges=new LinkedList<>();
        for (Vertex<V> vertex : graph.getVertices())
            edges.addAll(graph.getEdges(vertex.getValue()));
@@ -32,15 +45,25 @@ public class BellmanFord<V, E extends Number>  {
        for (int i = 0; i < graph.size()-1; i++) {
            for (Edge edge : edges) {
                Integer newPath=edge.getFrom().getWeight() +edge.getWeight().intValue();
-               if(edge.getTo().getWeight() > newPath){
+               if(edge.getFrom().getWeight()!=Integer.MAX_VALUE &&  edge.getTo().getWeight() > newPath){
                    edge.getTo().setWeight(newPath);
                    edge.getTo().setParent(edge.getFrom());
                }
            }
        }
-       //check negative cycle
-
-
+       /**
+        * Step 3 :
+        * check negative cycle
+        * the above guarantees shortest path, if there another shortest path found that means
+        * there is a negative cycle
+        * */
+       for (Edge edge : edges) {
+           Integer newPath=edge.getFrom().getWeight() +edge.getWeight().intValue();
+           if(edge.getFrom().getWeight()!=Integer.MAX_VALUE &&  edge.getTo().getWeight() > newPath){
+               System.out.println("Negative Cycle found");
+               break;
+           }
+       }
 
    }
 
