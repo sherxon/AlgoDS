ac6fcd8cded410ba766b9f6eae7058e63f92b81c
sherxon <sheraliobidov1199@gmail.com>
2017-01-18 01:57:23 -0600

    reorder list, sort colors

--- src/interviewquestions/medium/ReorderList.java
+++ src/interviewquestions/medium/ReorderList.java
@@ -6,40 +6,40 @@ import interviewquestions.utils.ListNode;
  * Created by sherxon on 1/8/17.
  */
 public class ReorderList {
-    ListNode r = null;
-    boolean done = false;
-
-    public static void main(String[] args) {
-        ListNode listNode=new ListNode(1);
-        listNode.next=new ListNode(2);
-        listNode.next.next=new ListNode(3);
-      //  listNode.next.next.next=new ListNode(4);
-      //  listNode.next.next.next.next=new ListNode(5);
-        //listNode.next.next.next.next.next=new ListNode(6);
-        new ReorderList().reorderList(listNode);
-        System.out.println(listNode);
-    }
 
     public void reorderList(ListNode head) {
         if(head==null)return;
-        r=head;
-        reorder(head);
-    }
-    void reorder(ListNode head){
-        if(head==null)return;
-        reorder(head.next);
-        if(r==head && !done){
-            done=true;
-            head.next=null;
-        }else if(r.next==head && !done){
-            done=true;
-            head.next=null;
-        } else if(!done){
-            ListNode temp=r.next;
-            r.next=head;
-            r.next.next=temp;
-            r=temp;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode right = slow.next;
+        slow.next = null; // break the link
+
+        // reverse the right
+        ListNode nh = right;
+        while (right != null && right.next != null) {
+            ListNode temp = right.next;
+            right.next = temp.next;
+            temp.next = nh;
+            nh = temp;
+        }
+
+        ListNode xx = head;
+        while (xx != null) {
+            ListNode temp = xx.next;
+            xx.next = nh;
+            if (nh != null) {
+                nh = nh.next;
+                xx.next.next = temp;
+            }
+
+            xx = temp;
         }
     }
+
 }
 
---
+++ src/interviewquestions/medium/SortColors.java
@@ -0,0 +1,29 @@
+package interviewquestions.medium;
+
+/**
+ * Created by sherxon on 1/17/17.
+ */
+public class SortColors {
+    public void sortColors(int[] nums) {
+        int os = 0;
+        int ones = 0;
+        int twos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) os++;
+            else if (nums[i] == 1) ones++;
+            else twos++;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (os > 0) {
+                nums[i] = 0;
+                os--;
+            } else if (ones > 0) {
+                nums[i] = 1;
+                ones--;
+            } else {
+                nums[i] = 2;
+            }
+        }
+
+    }
+}
