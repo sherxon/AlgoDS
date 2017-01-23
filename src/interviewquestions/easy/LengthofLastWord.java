b0abd80f03dcf0d89372912b12d7475003b94606
sherxon <sheraliobidov1199@gmail.com>
2017-01-23 15:53:17 -0600

    last wordlength

---
+++ src/interviewquestions/easy/LengthofLastWord.java
@@ -0,0 +1,15 @@
+package interviewquestions.easy;
+
+/**
+ * Created by sherxon on 1/22/17.
+ */
+public class LengthofLastWord {
+    public int lengthOfLastWord(String s) {
+        if (s == null || s.length() == 0) return 0;
+        s = s.trim();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') return s.length() - i - 1;
+        }
+        return s.length();
+    }
+}
