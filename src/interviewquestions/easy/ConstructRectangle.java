1c59fc73237ad45eab6bcea6adde4b7e5022c8bb
sherxon <sheraliobidov1199@gmail.com>
2017-01-22 20:23:00 -0600

    construct rengtangle

---
+++ src/interviewquestions/easy/ConstructRectangle.java
@@ -0,0 +1,23 @@
+package interviewquestions.easy;
+
+/**
+ * Created by sherxon on 1/22/17.
+ */
+public class ConstructRectangle {
+    public int[] constructRectangle(int area) {
+        if (area < 1) return new int[0];
+        if (area == 1) return new int[]{1, 1};
+        int[] res = new int[2];
+        int diff = Integer.MAX_VALUE;
+        int ii = 1;
+        for (int i = 2; i <= area / 2; i++) {
+            if (area % i == 0) {
+                if (diff > Math.abs(area / i - i)) {
+                    diff = Math.abs(area / i - i);
+                    ii = i;
+                }
+            }
+        }
+        return new int[]{Math.max(ii, area / ii), Math.min(ii, area / ii)};
+    }
+}
