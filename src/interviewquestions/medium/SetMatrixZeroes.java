4ad0eec9d298fa13b4e10e93877cb788c1721c1a
sherxon <sheraliobidov1199@gmail.com>
2017-01-27 00:20:46 -0600

    set matrix zeros

--- src/interviewquestions/medium/SetMatrixZeroes.java
+++ src/interviewquestions/medium/SetMatrixZeroes.java
@@ -1,11 +1,14 @@
 package interviewquestions.medium;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by sherxon on 1/26/17.
  */
 public class SetMatrixZeroes {
     public static void main(String[] args) {
-        int[][] a = new int[][]{{1, 2, 3, 4}, {0, 6, 7, 8}, {9, 8, 7, 6}};
+        int[][] a = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 8, 7, 0}};
         setZeroes(a);
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
@@ -17,11 +20,16 @@ public class SetMatrixZeroes {
 
     static void setZeroes(int[][] a) {
 
-        boolean first = a[0][0] == 0;
+        boolean firstr = false;
+        boolean firstc = false;
+        Set<Integer> cols = new HashSet<>();
 
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
                 if (a[i][j] == 0) {
+                    if (i == 0) firstr = true;
+                    if (j == 0) firstc = true;
+                    cols.add(j);
                     a[i][0] = 0;
                     a[0][j] = 0;
                 }
@@ -31,21 +39,19 @@ public class SetMatrixZeroes {
             if (a[i][0] == 0)
                 for (int j = 0; j < a[i].length; j++)
                     a[i][j] = 0;
+            else {
+                for (Integer col : cols)
+                    a[i][col] = 0;
+            }
         }
-        for (int i = 0; i < a[0].length; i++) {
-            if (a[0][i] == 0 && (i != 0 || first))
-                for (int j = 0; j < a.length; j++)
-                    a[j][i] = 0;
-        }
-        if (a[0][0] == 0 && false) {
+
+        if (firstr) {
             for (int j = 0; j < a[0].length; j++)
                 a[0][j] = 0;
-
-            if (first)
-                for (int j = 0; j < a.length; j++)
-                    a[j][0] = 0;
-
         }
+        if (firstc)
+            for (int j = 0; j < a.length; j++)
+                a[j][0] = 0;
 
     }
 }
