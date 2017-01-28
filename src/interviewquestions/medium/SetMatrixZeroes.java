7bdb7e9bda7fef2506b8ea6f98c96ccb313f77d5
sherxon <sheraliobidov1199@gmail.com>
2017-01-28 13:59:09 -0600

    game of life

--- src/interviewquestions/medium/SetMatrixZeroes.java
+++ src/interviewquestions/medium/SetMatrixZeroes.java
@@ -7,16 +7,7 @@ import java.util.Set;
  * Created by sherxon on 1/26/17.
  */
 public class SetMatrixZeroes {
-    public static void main(String[] args) {
-        int[][] a = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 8, 7, 0}};
-        setZeroes(a);
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+
 
     static void setZeroes(int[][] a) {
 
