d06c6a93ca73c85efb3e2a5daad2c9e0bccd359c
sherxon <sheraliobidov1199@gmail.com>
2017-03-19 14:34:19 -0500

    convert BST to greater Tree

---
+++ src/interviewquestions/medium/ConvertBSTToGreaterTree.java
@@ -0,0 +1,22 @@
+package interviewquestions.medium;
+
+import interviewquestions.utils.TreeNode;
+
+/**
+ * Created by sherxon on 3/19/17.
+ */
+public class ConvertBSTToGreaterTree {
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) return null;
+        f(root, 0);
+        return root;
+    }
+
+    int f(TreeNode root, int val) {
+        if (root == null) return val;
+        int r = f(root.right, val);
+        root.val = r + root.val;
+        int l = f(root.left, root.val);
+        return l;
+    }
+}
