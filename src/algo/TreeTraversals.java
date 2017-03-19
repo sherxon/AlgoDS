8dc0b209e63c220bf5c2bbe174a956cb5336484c
sherxon <sheraliobidov1199@gmail.com>
2017-03-19 14:32:47 -0500

    level order tree traversal

--- src/algo/TreeTraversals.java
+++ src/algo/TreeTraversals.java
@@ -2,16 +2,19 @@ package algo;
 
 import interviewquestions.utils.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by sherxon on 1/3/17.
  */
 public class TreeTraversals {
     public static void main(String[] args) {
-        TreeNode root=new TreeNode(1);
-        root.left=new TreeNode(2);
-        root.right=new TreeNode(3);
-        root.left.left=new TreeNode(4);
-        root.left.right=new TreeNode(5);
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
         System.out.println("InOrder");
         inOrder(root);
         System.out.println();
@@ -22,28 +25,44 @@ public class TreeTraversals {
         postOrder(root);
     }
 
-    private static void postOrder(TreeNode root) {
-        if(root==null)return;
+    public static void postOrder(TreeNode root) {
+        if (root == null) return;
         postOrder(root.left);
         postOrder(root.right);
         System.out.print(root.val + " ");
     }
 
-    private static void preOrder(TreeNode root) {
-        if(root==null)return;
-        // logic goes first
+    public static void preOrder(TreeNode root) {
+        if (root == null) return;
         System.out.print(root.val + " ");
         preOrder(root.left);
         preOrder(root.right);
     }
 
-    private static void inOrder(TreeNode root) {
-        if(root==null)return;
+    public static void inOrder(TreeNode root) {
+        if (root == null) return;
         inOrder(root.left);
-        // logic goes here
         System.out.print(root.val + " ");
-
         inOrder(root.right);
+    }
 
+    public void levelOrder(TreeNode root) {
+        if (root == null) return;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root); // add first level to queue
+        int level = 1;
+        while (!q.isEmpty()) {
+            TreeNode x = q.remove();
+            level--;
+            if (x.left != null)
+                q.add(x.left);
+            if (x.right != null)
+                q.add(x.right);
+
+            if (level == 0 && !q.isEmpty()) {
+                level += q.size();
+                System.out.println(q);
+            }
+        }
     }
 }
