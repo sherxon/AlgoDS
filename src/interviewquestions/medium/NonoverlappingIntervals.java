3ef1dfe7c6fe8c96277ccdb45694d0ea7dbe62e1
sherxon <sheraliobidov1199@gmail.com>
2017-04-05 13:56:04 -0500

    non overlapping intervals

---
+++ src/interviewquestions/medium/NonoverlappingIntervals.java
@@ -0,0 +1,27 @@
+package interviewquestions.medium;
+
+import interviewquestions.utils.Interval;
+
+import java.util.Arrays;
+
+/**
+ * Created by sherxon on 4/5/17.
+ */
+public class NonoverlappingIntervals {
+    public int eraseOverlapIntervals(Interval[] intervals) {
+        int count = 0;
+        Arrays.sort(intervals, (a, b) -> {
+            int count1 = a.start - b.start;
+            if (count1 == 0)
+                count1 = a.end - b.end;
+            return count1;
+        });
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i - 1].end > intervals[i].start) {
+                intervals[i].end = Math.min(intervals[i - 1].end, intervals[i].end);
+                count++;
+            }
+        }
+        return count;
+    }
+}
