95c8fc32a82cd4a308d422c9b262a89c8fe00ccd
sherxon <sheraliobidov1199@gmail.com>
2017-04-15 15:54:54 -0500

    shortest word distance 2 and 3

---
+++ src/interviewquestions/medium/ShortestWordDistance2.java
@@ -0,0 +1,32 @@
+package interviewquestions.medium;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by sherxon on 4/15/17.
+ */
+public class ShortestWordDistance2 {
+
+    Map<String, Set<Integer>> map= new HashMap<>();
+
+    public ShortestWordDistance2(String[] words) {
+        for(int i=0; i<words.length; i++){
+            if(!map.containsKey(words[i]))
+                map.put(words[i], new HashSet<>());
+            map.get(words[i]).add(i);
+        }
+    }
+
+    public int shortest(String word1, String word2) {
+        int min=Integer.MAX_VALUE;
+        for(Integer i:map.get(word1)){
+            for(Integer j:map.get(word2)){
+                min=Math.min(min, Math.abs(i-j));
+            }
+        }
+        return min;
+    }
+}
---
+++ src/interviewquestions/medium/ShortestWordDistance3.java
@@ -0,0 +1,33 @@
+package interviewquestions.medium;
+
+/**
+ * Created by sherxon on 4/15/17.
+ */
+public class ShortestWordDistance3 {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        int min=Integer.MAX_VALUE;
+        if(!word1.equals(word2)){
+            int index1=-1;
+            int index2=-1;
+            for(int i=0; i<words.length; i++){
+                if(words[i].equals(word1))index1=i;
+                else if(words[i].equals(word2))index2=i;
+                if(index1!=-1 && index2!=-1)
+                    min=Math.min(min, Math.abs(index1-index2));
+            }
+        }else{
+            int p=-1;
+            int cur=-1;
+            for(int i=0; i<words.length; i++){
+                if(words[i].equals(word1)){
+                    p=cur;
+                    cur=i;
+                }
+                if(p!=-1 && cur!=-1)
+                    min=Math.min(min, cur-p);
+            }
+        }
+        return min;
+
+    }
+}
