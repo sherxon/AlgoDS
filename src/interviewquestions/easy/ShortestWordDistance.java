f4224ce873a8200e283ab85fb8d39a90f24eb44e
sherxon <sheraliobidov1199@gmail.com>
2017-04-15 14:15:20 -0500

    shortest word distance

---
+++ src/interviewquestions/easy/ShortestWordDistance.java
@@ -0,0 +1,23 @@
+package interviewquestions.easy;
+
+/**
+ * Created by sherxon on 4/15/17.
+ */
+public class ShortestWordDistance {
+    public static void main(String[] args) {
+        new ShortestWordDistance().shortestDistance(new String[]{"a","b","c","d","d"},"a", "d");
+    }
+
+    public int shortestDistance(String[] a, String word1, String word2) {
+        int index1=-1;
+        int index2=-1;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<a.length; i++){
+            if(word1.equals(a[i]))index1=i;
+            else if(word2.equals(a[i]))index2=i;
+            if(index1!=-1 && index2!=-1)
+                min=Math.min(min, Math.abs(index1-index2));
+        }
+        return min;
+    }
+}
