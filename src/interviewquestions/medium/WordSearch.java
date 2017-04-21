be1dbf1bdb91eac1b64fec52f7c6d1ade1893b2e
sherxon <sheraliobidov1199@gmail.com>
2017-04-20 20:18:58 -0500

    word search optimized solution with global variable

--- src/interviewquestions/medium/WordSearch.java
+++ src/interviewquestions/medium/WordSearch.java
@@ -4,7 +4,9 @@ package interviewquestions.medium;
  * Created by sherxon on 1/11/17.
  */
 public class WordSearch {
-
+    /**
+    * This is recursive solution without using global variable
+    * */
     boolean exist(char[][] a, String word) {
         int r = 1;
         for (int i = 0; i < a.length; i++) {
@@ -52,5 +54,40 @@ public class WordSearch {
             a[i - 1][j] = temp;
         }
         return r;
+    }
+    /**
+    * This is cute solution using global variable
+    * */
+     class Solution {
+        private boolean found=false;
+
+        public boolean exist(char[][] a, String word) {
+            char[] s=word.toCharArray();
+
+            for(int i=0; i<a.length && !found; i++){
+                for(int j=0; j<a[i].length && !found; j++){
+                    if(a[i][j]==s[0])
+                        go(a, i, j, s, 0);
+                }
+            }
+            return found;
+        }
+        void go(char[][]a, int i, int j, char[] s, int index){
+            if(i<0 || j<0 || i>=a.length || j>=a[i].length || index >= s.length) return;
+
+            if(s[index]!=a[i][j] || found)return;
+
+            if(index==s.length-1){
+                found=true;
+                return;
+            }
+            char temp=a[i][j];
+            a[i][j]='.';
+            go(a, i+1, j, s, index+1);
+            go(a, i, j+1, s, index+1);
+            go(a, i-1, j, s, index+1);
+            go(a, i, j-1, s, index+1);
+            a[i][j]=temp;
+        }
     }
 }
