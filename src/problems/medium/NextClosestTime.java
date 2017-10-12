507c796f985a0e65d1050d4ab837e655f5d4833b
Sherali Obidov <sobidov@spins.com>
2017-10-12 10:32:40 -0500

    next closest time problem and solution

--- src/problems/medium/NextClosestTime.java
+++ src/problems/medium/NextClosestTime.java
@@ -7,20 +7,22 @@ public class NextClosestTime {
     public static void main(String[] args) {
         System.out.println(new NextClosestTime().nextClosestTime("19:34"));
     }
+
     public String nextClosestTime(String time) {
-        int n=Integer.parseInt(time.split(":")[0]+time.split(":")[1]);
-        for(int i=n/100; i<24; i++){
-            int k=n%100+1;
-            if(i>n/100)k=0;
-            for(int j=k; j<60; j++){
-                if(contains(time, i, j)){
+        int n = Integer.parseInt(time.split(":")[0] + time.split(":")[1]);
+        for (int i = n / 100; i < 24; i++) {
+            int k = n % 100 + 1;
+            if (i > n / 100)
+                k = 0;
+            for (int j = k; j < 60; j++) {
+                if (contains(time, i, j)) {
                     return make(i, j);
                 }
             }
         }
-        for(int i=0; i<n/100; i++){
-            for(int j=0; j<n%100; j++){
-                if(contains(time, i, j)){
+        for (int i = 0; i < n / 100; i++) {
+            for (int j = 0; j < n % 100; j++) {
+                if (contains(time, i, j)) {
                     return make(i, j);
                 }
             }
@@ -28,22 +30,27 @@ public class NextClosestTime {
         return time;
 
     }
-    String make(int i, int j){
-        String l= i < 10 ? "0"+i : i+"";
-        String r= j < 10 ? "0"+j : j+"";
-        return l+":"+r;
+
+    String make(int i, int j) {
+        String l = i < 10 ? "0" + i : i + "";
+        String r = j < 10 ? "0" + j : j + "";
+        return l + ":" + r;
 
     }
-    boolean contains(String s, int i, int j){
-        if((i<10 || j<10) && !s.contains("0")) return false;
-        while(i!=0){
-            if(!s.contains(i%10+""))return false;
-            i/=10;
+
+    boolean contains(String s, int i, int j) {
+        if ((i < 10 || j < 10) && !s.contains("0"))
+            return false;
+        while (i != 0) {
+            if (!s.contains(i % 10 + ""))
+                return false;
+            i /= 10;
         }
 
-        while(j!=0){
-            if(!s.contains(j%10+""))return false;
-            j/=10;
+        while (j != 0) {
+            if (!s.contains(j % 10 + ""))
+                return false;
+            j /= 10;
         }
 
         return true;
