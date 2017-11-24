8f85c97280aeec2c5c89f69409e65f78d8ba0029
Sherali Obidov <sobidov@spins.com>
2017-11-23 18:44:26 -0600

    superstack

--- src/toptal/Third.java
+++ src/toptal/Third.java
@@ -8,6 +8,7 @@ public class Third {
         // write your code in Java SE 8
         // 1,5,4,9,8,7,12,13,14
         // 0,1,0,2,1,0,0,0,0
+        // 1,2,3,4,5,6,0
         if (a == null || a.length == 0)
             return 0;
         if (a.length == 1)
