7925f941599b503212d4d70f4daa7c789bdc3c0b
PiyushPawar17 <piyushpawar25@gmail.com>
2017-12-14 21:54:21 +0530

    Added Description

--- src/algo/sortingandsearching/HeapSort.java
+++ src/algo/sortingandsearching/HeapSort.java
@@ -1,14 +1,31 @@
+/*
+ * Heap sort is a sorting algorithm that works by first organizing the data to be sorted into a special type of binary tree called a heap. 
+ * The heap itself has, by definition, the largest value at the top of the tree, so the heap sort algorithm must also reverse the order. 
+ * It does this with the following steps:
+ * 1. Remove the topmost item (the largest) and replace it with the rightmost leaf. The topmost item is stored in an array.
+ * 2. Re-establish the heap.
+ * 3. Repeat steps 1 and 2 until there are no more items left in the heap.
+ * The sorted elements are now stored in an array.
+ * A heap sort is especially efficient for data that is already stored in a binary tree.
+ * ----------------------------------------------------------------------------------------------------------------------------------------
+ * Time Complexity : O(n log(n))
+ * Space Complexity : O(1)
+ */
+
 public class HeapSort {
+	// Method use to build max heap. Max heap is a heap where value of all root elements in every sub-tree are greater than that of child.
 	private static void heapify(int[] arr, int root, int heapSize) {
-		int maximum = root;
+		int maximum = root;					// Assuming that root is the maximum
 		int leftChild = 2 * root + 1;
 		int rightChild = 2 * root + 2;
 
+		// Checking which element among root, left child and right child has maximum value.
 		if (leftChild < heapSize && arr[leftChild] > arr[root])
 			maximum = leftChild;
 		if (rightChild < heapSize && arr[rightChild] > arr[maximum])
 			maximum = rightChild;
 
+		// If root does not have the maximum value, swap with the maximum value and again build a max heap in the sub-tree from the swapped index.
 		if (root != maximum) {
 			int temp = arr[root];
 			arr[root] = arr[maximum];
@@ -19,8 +36,9 @@ public class HeapSort {
 
 	private static void heapSort(int[] arr, int heapSize) {
 		for (int i = heapSize / 2 - 1; i >= 0; i--)
-			heapify(arr, i, heapSize);
+			heapify(arr, i, heapSize);	// Iterating through the array to build the max heap.
 
+		// Swapping the root (the largest element) with the last index and then again building max heap of the remaining array.
 		for (int i = heapSize - 1; i > 0; i--) {
 			int temp = arr[i];
 			arr[i] = arr[0];
