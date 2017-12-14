a1326fd8e616e447b3a504bb689225fd89d067f1
Sherali Obidov <sobidov@spins.com>
2017-12-14 14:33:09 -0600

    added missed package statements

--- src/algo/numerals/BabylonianSquareRoot.java
+++ src/algo/numerals/BabylonianSquareRoot.java
@@ -1,3 +1,4 @@
+package algo.numerals;
 /*
  * Babylonian Sqaure Root is a method to find Square Root of a number by simple mathematical operations.
  * The steps involved in calculating the square root are :
@@ -10,23 +11,23 @@
 
 public class BabylonianSquareRoot {
 
-	private static float babylonianSquareRoot(float num) throws ArithmeticException {
-		
-		if (num < 0)
-			throw new ArithmeticException("NaN");
-		float x = num;
-		float y = 1; 
-		float e = 0.00000001f;	// e decides the accuracy level. Smaller the value of e, more is the answer accurate, but also more are the steps involved
+    private static float babylonianSquareRoot(float num) throws ArithmeticException {
 
-		// Looping while the difference between x and y is greater than e
-		while (x - y > e) {
-			x = (x + y) / 2;	// Updating x with the average of x and y
-			y = num / x;		// Updating y with original number divided by the average
-		}
-		return x;
-	}
+        if (num < 0)
+            throw new ArithmeticException("NaN");
+        float x = num;
+        float y = 1;
+        float e = 0.00000001f;    // e decides the accuracy level. Smaller the value of e, more is the answer accurate, but also more are the steps involved
 
-	public static void main(String[] args) {
-		System.out.println(babylonianSquareRoot(52));	
-	}
+        // Looping while the difference between x and y is greater than e
+        while (x - y > e) {
+            x = (x + y) / 2;    // Updating x with the average of x and y
+            y = num / x;        // Updating y with original number divided by the average
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(babylonianSquareRoot(52));
+    }
 }
--- src/algo/recursion/Factorial.java
+++ src/algo/recursion/Factorial.java
@@ -1,3 +1,4 @@
+package algo.recursion;
 /*
  * Factorial is the product of all numbers from 1 to n.
  * Example : 5! = 5 x 4 x 3 x 2 x 1 = 120
@@ -6,13 +7,13 @@
 
 public class Factorial {
 
-	private static long factorial(int num) {
-		if (num <= 1)		// 1! = 1, hence return 1 when the num becomes 1.
-			return 1;
-		return num * factorial(num - 1); // num! = num * (num - 1)!
-	}
+    private static long factorial(int num) {
+        if (num <= 1)        // 1! = 1, hence return 1 when the num becomes 1.
+            return 1;
+        return num * factorial(num - 1); // num! = num * (num - 1)!
+    }
 
-	public static void main(String[] args) {
-		System.out.println(factorial(10));	
-	}
+    public static void main(String[] args) {
+        System.out.println(factorial(10));
+    }
 }
