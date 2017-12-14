7c6c0073e24cd313bcd3baa62966ff4b7da3f0bb
Sherali Obidov <sobidov@spins.com>
2017-12-14 14:27:49 -0600

    added missing package statements

--- src/algo/recursion/Ackermann.java
+++ src/algo/recursion/Ackermann.java
@@ -1,4 +1,5 @@
-/*
+package algo.recursion;
+/**
  * the Ackermann function is one of the simplest and earliest-discovered examples of a total computable function that is not primitive recursive.
  * All primitive recursive functions are total and computable, but the Ackermann function illustrates that not all total computable functions are primitive recursive.
  * Ackermann Function is given by :
@@ -12,12 +13,13 @@ import java.math.BigInteger;
 
 // This method may give Stack Overflow Exception
 public class Ackermann {
-	
-	private static BigInteger ackermann(BigInteger m, BigInteger n) {
-		return m.equals(BigInteger.ZERO) ? n.add(BigInteger.ONE) : ackermann(m.subtract(BigInteger.ONE), n.equals(BigInteger.ZERO) ? BigInteger.ONE : ackermann(m, n.subtract(BigInteger.ONE)));
-	}
 
-	public static void main(String[] args) {
-		System.out.println(ackermann(new BigInteger("2"), new BigInteger("2")));
-	}
+    private static BigInteger ackermann(BigInteger m, BigInteger n) {
+        return m.equals(BigInteger.ZERO) ? n.add(BigInteger.ONE) :
+                ackermann(m.subtract(BigInteger.ONE), n.equals(BigInteger.ZERO) ? BigInteger.ONE : ackermann(m, n.subtract(BigInteger.ONE)));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ackermann(new BigInteger("2"), new BigInteger("2")));
+    }
 }
--- src/algo/recursion/TowerOfHanoi.java
+++ src/algo/recursion/TowerOfHanoi.java
@@ -1,4 +1,6 @@
-/*
+package algo.recursion;
+
+/**
  * Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. 
  * The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
  * 1) Only one disk can be moved at a time.
@@ -48,18 +50,18 @@
  */
 
 public class TowerOfHanoi {
-	public static void towerOfHanoi(int top, char start, char via, char end) {
-		if (top == 1) 
-			System.out.println(start + " --> " + end);	// If one disk is left, move it to last rod
-		else {
-			towerOfHanoi(top - 1, start, end, via);		// Shifting n - 1 disks from A to B with the help of C
-			System.out.println(start + " --> " + end);	// Shifting last disk from A to C
-			towerOfHanoi(top - 1, via, start, end);		// Shifting n - 1 disks from B to C with the help of A
-		}
-	}
+    public static void towerOfHanoi(int top, char start, char via, char end) {
+        if (top == 1)
+            System.out.println(start + " --> " + end);    // If one disk is left, move it to last rod
+        else {
+            towerOfHanoi(top - 1, start, end, via);        // Shifting n - 1 disks from A to B with the help of C
+            System.out.println(start + " --> " + end);    // Shifting last disk from A to C
+            towerOfHanoi(top - 1, via, start, end);        // Shifting n - 1 disks from B to C with the help of A
+        }
+    }
 
-	public static void main(String[] args) {
-		int blocks = 5;							// Number of blocks in the tower 
-		towerOfHanoi(blocks, 'A', 'B', 'C');	// Moving all blocks from A to C
-	}
+    public static void main(String[] args) {
+        int blocks = 5;                            // Number of blocks in the tower
+        towerOfHanoi(blocks, 'A', 'B', 'C');    // Moving all blocks from A to C
+    }
 }
