266c671788c9c441ed84369e2016f82b9252b278
PiyushPawar17 <piyushpawar25@gmail.com>
2017-12-14 23:53:38 +0530

    Added Description

--- src/algo/numerals/Permutation.java
+++ src/algo/numerals/Permutation.java
@@ -9,7 +9,7 @@ public class Permutation {
 
 	public static long npr(int n, int r) {
 
-		if (r > n || n < 0 || r < 0) 		// If r is greater than n or n or r is less than 0, then the permutations are not possible
+		if (r > n || n < 0 || r < 0) 		// If r is greater than n or n or r is less than 0, then the permutations are not possible.
 			return -1;
 		long ans = 1;
 		for (int i = n; i > n - r; i--) 	// Loop to find product of numbers from n to n - r as 1 to r-1 numbers are cancelled in the formula of nPr
