47c023e67f9d95888d6682ea5401d8ab181197ba
PiyushPawar17 <piyushpawar25@gmail.com>
2017-12-14 22:37:24 +0530

    Added Description

--- src/algo/numerals/ModularExponential.java
+++ src/algo/numerals/ModularExponential.java
@@ -1,14 +1,22 @@
+/*
+ * Modular exponentiation is a type of exponentiation performed over a modulus. It is useful in computer science, especially in the field of public-key cryptography.
+ * Given three numbers x, y and p, compute (x^y) % p.
+ * For every odd power, the result (which is initialized to 1) is updated as (result * base) % mod.
+ * Then the power is divided by 2 and the base is updated to (base * base) % mod.
+ * This is done till the power does not become 0.
+ */
+
 public class ModularExponential {
 
 	private static int modularExponential(int base, int power, int mod) {
 
 		int result = 1;
-		base %= mod;
+		base %= mod; 	// Update the base value if it is more than or equal to mod value
 
 		while (power > 0) {
-			if (power % 2 == 1)
+			if (power % 2 == 1)		// If power is odd, multiply update the result as below
 				result = (result * base) % mod;
-			power = power >> 1;
+			power = power >> 1;		// power /= 2
 			base = (base * base) % mod;
 		}
 
