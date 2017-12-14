5b0fbaf1814f909fed24f3bf9df63c6b9aaca4af
PiyushPawar17 <piyushpawar25@gmail.com>
2017-12-14 22:46:57 +0530

    Added Description

--- src/algo/numerals/BabylonianSquareRoot.java
+++ src/algo/numerals/BabylonianSquareRoot.java
@@ -1,3 +1,13 @@
+/*
+ * Babylonian Sqaure Root is a method to find Square Root of a number by simple mathematical operations.
+ * The steps involved in calculating the square root are :
+ * 1) Start with an arbitrary positive start value x (the closer to the root, the better).
+ * 2) Initialize y = 1.
+ * 3) Do following until desired approximation is achieved.
+ *		a) Get the next approximation for root using average of x and y
+ *		b) Set y = n/x
+ */
+
 public class BabylonianSquareRoot {
 
 	private static float babylonianSquareRoot(float num) throws ArithmeticException {
@@ -6,11 +16,12 @@ public class BabylonianSquareRoot {
 			throw new ArithmeticException("NaN");
 		float x = num;
 		float y = 1; 
-		float e = 0.00000001f;
+		float e = 0.00000001f;	// e decides the accuracy level. Smaller the value of e, more is the answer accurate, but also more are the steps involved
 
+		// Looping while the difference between x and y is greater than e
 		while (x - y > e) {
-			x = (x + y) / 2;
-			y = num / x;
+			x = (x + y) / 2;	// Updating x with the average of x and y
+			y = num / x;		// Updating y with original number divided by the average
 		}
 		return x;
 	}
