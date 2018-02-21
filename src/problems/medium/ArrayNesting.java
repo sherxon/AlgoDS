9d59b89d643359e586e7611c2a9ddf98d98d8b47
Sherali Obidov <sobidov@spins.com>
2018-02-21 09:20:25 -0600

    optimized

--- src/problems/medium/ArrayNesting.java
+++ src/problems/medium/ArrayNesting.java
@@ -11,20 +11,17 @@ public class ArrayNesting {
     }
 
     int[] a = new int[nums.length];
-    int counter = 0;
+    int counter = 1;
     int max = 0;
     for (int i = 0; i < a.length; i++) {
-      if (a[i] > 0) {
-        continue;
-      }
       int j = i;
-      counter++;
       int k = 0;
       while (a[j] == 0) {
         a[j] = counter;
         j = nums[j];
         k++;
       }
+      counter++;
       max = Math.max(k, max);
     }
     return max;
