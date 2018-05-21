08dcd9cf36eb7ba5d483af4069a497767b8c8dd1
Sherali <sobidov@spins.com>
2018-05-21 18:19:10 -0500

    amazon demo questions

---
+++ src/temp/Demo2.java
@@ -0,0 +1,36 @@
+package temp;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Why Did you create this class? what does it do?
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+
+    }
+
+    // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+    public int generalizedGCD(int num, int[] arr) {
+        // WRITE YOUR CODE HERE
+        if (num <= 0 || arr == null || arr.length == 0)
+            return 0;
+        if (arr.length == 1)
+            return arr[0];
+        int gcdOfTwo = getGCD(arr[0], arr[1]);
+        for (int i = 2; i < arr.length; i++) {
+            gcdOfTwo = getGCD(gcdOfTwo, arr[i]);
+        }
+        return gcdOfTwo;
+
+    }
+
+    int getGCD(int x, int y) {
+        if (y == 0)
+            return x;
+        return getGCD(y, x % y);
+    }
+
+    // METHOD SIGNATURE ENDS
+}
