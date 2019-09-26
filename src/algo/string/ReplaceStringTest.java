9e0e0ffe92aa5b526316ecaca6909b129094906a
duong17020661 <43227742+duong17020661@users.noreply.github.com>
2019-09-26 22:46:23 +0700

    update test MDMC test

--- src/algo/string/ReplaceStringTest.java
+++ src/algo/string/ReplaceStringTest.java
@@ -7,18 +7,19 @@ class ReplaceStringTest {
     @Test
     void testReplaceAll() {
         String s1 = "abcabc";
-        String s2 = "bc";
+        String s2 = "b";
         String s3 = "1";
-        String s4 = "@";
-        String s8 = " ";
-        assertEquals("1bc1bc123123@@@", algo.string.ReplaceString.replaceAll(s1,s2,s3));
-        assertEquals("abcabca23a23@@@", algo.string.ReplaceString.replaceAll(s1,s3,s2));
-        assertEquals("abcabc123123111", algo.string.ReplaceString.replaceAll(s1,s4,s3));
-        assertEquals("abcabc@23@23@@@", algo.string.ReplaceString.replaceAll(s1,s3,s4));
-        assertEquals("abcabc123123aaa", algo.string.ReplaceString.replaceAll(s1,s4,s2));
-        assertEquals("@bc@bc123123@@@", algo.string.ReplaceString.replaceAll(s1,s2,s4));
-        assertEquals(" bc bc123123@@@", algo.string.ReplaceString.replaceAll(s1,s2,s8));
-        assertEquals("abcabc123123@@@", algo.string.ReplaceString.replaceAll(s1," "," "));
+        String s4 = "abc";
+        String s5 = "123";
+        String s8 = "";
+        assertEquals("a1ca1c", algo.string.ReplaceString.replaceAll(s1,s2,s3)); // Kiem thu luong du lieu
+        assertEquals("123123", algo.string.ReplaceString.replaceAll(s1,s4,s5));
+        assertEquals("", algo.string.ReplaceString.replaceAll(s1,s4,s8));
+        assertEquals("11", algo.string.ReplaceString.replaceAll(s1,s4,s3));
+        assertEquals("acac", algo.string.ReplaceString.replaceAll(s1,s2,s8));
+        assertEquals("acac", algo.string.ReplaceString.replaceAll(s1,s2,s8));
+//        assertEquals(" bc bc123123@@@", algo.string.ReplaceString.replaceAll(s1,s2,s8));
+//        assertEquals("abcabc123123@@@", algo.string.ReplaceString.replaceAll(s1," "," "));
 
     }
 }
