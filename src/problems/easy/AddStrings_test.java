fdce1623ff593648c3ab2a8ee2adf2eb13338e0a
vietcuongk99 <daoquocthai8@gmail.com>
2019-09-28 01:05:03 +0700

    Update AddStrings_test.java
    - improve test case

--- src/problems/easy/AddStrings_test.java
+++ src/problems/easy/AddStrings_test.java
@@ -3,17 +3,66 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AddStringsTest {
-    @Test
-    void testAddStrings() {
-        String a = "";
-        String b = "";
-        String c = "00033";
-        String d = "010100";
         
-        assertEquals("", problems.easy.AddStrings.addStrings(a, b));
-        assertEquals("00033", problems.easy.AddStrings.addStrings(a, c));
-        assertEquals("010100", problems.easy.AddStrings.addStrings(b, d));
-        assertEquals("010133", problems.easy.AddStrings.addStrings(c, d));
+        /*----  Unit test case ---*/
+        @Test
+        void unitTestcase1() {
+            String c = "00033";
+            String d = "010100";
+            String u1 =  problems.easy.AddStrings.addStrings(c, d);
+            assertEquals("010133", u1);
 
-    }
+        }
+
+        @Test
+        void unitTestcase2() {
+            String c = "00033";
+            String e = "12345";
+            String u2 =  problems.easy.AddStrings.addStrings(c, e);
+            assertEquals("12378", u2);
+
+        }
+
+        @Test
+        void unitTestcase3() {
+            String e = "12345";
+            String d = "010100";
+            String u3 =  problems.easy.AddStrings.addStrings(d, e);
+            assertEquals("022445", u3);
+
+        }
+
+
+        /*----  MCDC test  ---*/
+
+        @Test
+        // A=T, B=T, C=T
+        void MCDCTest1() {
+            String a = "112";
+            String b = "001";
+            String c =  problems.easy.AddStrings.addStrings(a, b);
+            assertEquals("113", c);
+
+        }
+
+        @Test
+        // A=F, B=T, C=T
+        void MCDCTest1() {
+            String a = "";
+            String b = "001";
+            String c =  problems.easy.AddStrings.addStrings(a, b);
+            assertEquals("001", c);
+
+        }
+
+        @Test
+        // A=T, B=F, C=T
+        void MCDCTest1() {
+            String a = "112";
+            String b = "";
+            String c =  problems.easy.AddStrings.addStrings(a, b);
+            assertEquals("112", c);
+
+        }
+    
 }
