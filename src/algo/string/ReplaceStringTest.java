15cd32dd9dbd83cb307a6d8910b05aee30b2a5ed
duong17020661 <43227742+duong17020661@users.noreply.github.com>
2019-09-27 20:51:47 +0700

    add all DU path test and MCDC test

--- src/algo/string/ReplaceStringTest.java
+++ src/algo/string/ReplaceStringTest.java
@@ -3,23 +3,61 @@ import org.junit.jupiter.api.Test;
 
         import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ReplaceStringTest {
+public class ReplaceStringTest {
+    /*-------------------Simple Unit Test------------------------*/
     @Test
-    void testReplaceAll() {
-        String s1 = "abcabc";
-        String s2 = "b";
-        String s3 = "1";
-        String s4 = "abc";
-        String s5 = "123";
-        String s8 = "";
-        assertEquals("a1ca1c", algo.string.ReplaceString.replaceAll(s1,s2,s3)); // Kiem thu luong du lieu
-        assertEquals("123123", algo.string.ReplaceString.replaceAll(s1,s4,s5));
-        assertEquals("", algo.string.ReplaceString.replaceAll(s1,s4,s8));
-        assertEquals("11", algo.string.ReplaceString.replaceAll(s1,s4,s3));
-        assertEquals("acac", algo.string.ReplaceString.replaceAll(s1,s2,s8));
-        assertEquals("acac", algo.string.ReplaceString.replaceAll(s1,s2,s8));
-//        assertEquals(" bc bc123123@@@", algo.string.ReplaceString.replaceAll(s1,s2,s8));
-//        assertEquals("abcabc123123@@@", algo.string.ReplaceString.replaceAll(s1," "," "));
-
+    void unitTest1(){
+        String s = "aaa";
+        assertEquals("111", algo.string.ReplaceString.replaceAll(s,"a","1"));
     }
+    @Test
+    void unitTest2(){
+        String s = "aaa";
+        assertEquals("   ", algo.string.ReplaceString.replaceAll(s,"a"," "));
+    }
+    @Test
+    void unitTest3(){
+        String s = "aaa";
+        assertEquals("aaa", algo.string.ReplaceString.replaceAll(s,"a","a"));
+    }
+    /*-------------------Some MCDC Test------------------------*/
+    @Test
+    void MCDCTest1(){
+        String s = "abcabc";
+        assertEquals("a1ca1c", algo.string.ReplaceString.replaceAll(s,"b","1"));
+        // TH: If(A)=T; If(B)=F; If(C) = T
+    }
+    @Test
+    void MCDCTest2(){
+        String s = "abcabc";
+        assertEquals("", algo.string.ReplaceString.replaceAll(s,"abc",""));
+        // TH: If(A)=f; If(B)=F; If(C) = T
+    }
+    @Test
+    void MCDCTest3(){
+        String s = "abcabc";
+        assertEquals("123123", algo.string.ReplaceString.replaceAll(s,"abc","123"));
+        // TH: If(A)=T; If(B)=T; If(C) = T
+    }
+    @Test
+        /*-------------------Some All DU path Test------------------------*/
+    void allDUpathTest1(){
+        String s = "abcabc";
+        assertEquals("1bc1bc", algo.string.ReplaceString.replaceAll(s,"a","1"));
+        // DU path cho s
+    }
+    @Test
+    void allDUpathTest2(){
+        String s = "abcabc";
+        assertEquals("123123", algo.string.ReplaceString.replaceAll(s,"abc","123"));
+        // DU path cho 1
+    }
+    @Test
+    void allDUpathTest3(){
+        String s = "abcabc";
+        assertEquals("", algo.string.ReplaceString.replaceAll(s,"abc",""));
+        // DU path cho 2
+    }
+
+
 }
